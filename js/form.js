9281e54bcfeb268badc57cbbb5176fb7e3b2ac30
Sylwia Ciszek <syl_kam@op.pl>
2021-05-21 09:04:33 +0200

    Initial commit

---
+++ js/form.js
@@ -0,0 +1,39 @@
+
+let formElement = document.querySelector(".js-form");
+let plnElement = document.querySelector(".js-plnValue");
+let currencyElement = document.querySelector(".js-currencyOption");
+let resultElement = document.querySelector(".js-result");
+
+let rateEUR = 4.56;
+let rateGBP = 5.31;
+let rateUSD = 3.81;
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let amount = +plnElement.value;
+    let currency = currencyElement.value;
+
+    let result;
+
+    switch (currency) {
+        case "EUR":
+            result = amount / rateEUR;
+            break;
+
+        case "GBP":
+            result = amount / rateGBP;
+            break;
+
+        case "USD":
+            result = amount / rateUSD;
+            break;
+
+    }
+
+    resultElement.innerText = `${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
+
+
+
+});
+
